bc1b35561c1120c7359a71462199804536cbbfec
Filipe <filipegallodev@hotmail.com>
2023-12-07 20:46:27 -0300

    add product controller, dto, entity, repository and service

--- Back-End/src/main/java/com/filipegallo/stock/config/WebConfig.java
+++ Back-End/src/main/java/com/filipegallo/stock/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.filipegallo.estoque.config;
+package com.filipegallo.stock.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
---
+++ Back-End/src/main/java/com/filipegallo/stock/dto/ProductDTO.java
@@ -0,0 +1,70 @@
+package com.filipegallo.stock.dto;
+
+import com.filipegallo.stock.entities.Product;
+import org.springframework.beans.BeanUtils;
+
+import java.time.LocalDate;
+
+public class ProductDTO {
+
+    private Long id;
+    private String name;
+    private Integer amount;
+    private String description;
+    private Boolean active;
+    private LocalDate lastChange;
+
+    public ProductDTO() {}
+
+    public ProductDTO(Product entity) {
+        BeanUtils.copyProperties(entity, this);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public LocalDate getLastChange() {
+        return lastChange;
+    }
+
+    public void setLastChange(LocalDate lastChange) {
+        this.lastChange = lastChange;
+    }
+}
